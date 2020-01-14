fadaf4515aab1009f4df4a1af5a2e8f82077bc4c
Sebastian Fricke <sebastian.fricke-linux@gmx.de>
2020-01-14 15:35:44 +0100

    Add module + test for the category-id config
    the module contains a class with the handling functions for finding and
    reading the configuration.
    There is currently one test for the readConfig function which checks
    for correct, missing and wrong input

--- packages/category.py
+++ packages/category.py
@@ -1,17 +1,59 @@
-from re import search
+from packages.item_upload import check_encoding
+from packages.log_files import noCategoryFileLog
+import os
+from pathlib import Path
 
-#It is possible to take the Categories from the Intern Sorting Numbers by splitting the string
-def determineCategory(name, producttype, gender, level):
-    output = ''
-    # Determine the first Category by checking the producttype
-    if(level == 1):
-        if(search(r'bags',producttype.lower())):
-            output = 'Bags'
-        elif(search(r'accessoires',producttype.lower())):
-            output = 'Accessoires'
-        elif(not(search(r'(accessoires|bags)',producttype.lower()))):
-            output = 'Bekleidung'
+class CategoryConfig:
+    def __init__(self, proj_root, log_path):
+        self.root = proj_root
+        self.log = log_path
+        self.id_list = dict()
+        self.raw_data = []
+        self.path = {'path':'', 'encoding':''}
+
+    def findConfig(self):
+        try:
+            self.path['path'] = os.path.join(self.root, 'category')
+        except Exception as err:
+            print("ERROR @ findCategoryConfig: Building path failed: {0}"
+                  .format(err))
+            return False
+
+        if(os.path.isfile(self.path['path'])):
+            self.path['encoding'] = check_encoding(self.path)
+            with open(self.path['path'],
+                      mode='r', encoding=self.path['encoding']) as config:
+                try:
+                    self.raw_data =\
+                    [row.stip(' ').strip('\n').split(';') for row in config]
+                except Exception as err:
+                    print("ERROR @ findCategoryConfig: Read data failed: {0}"
+                          .format(err))
+
+            return True
         else:
-            output = ''
-    elif(level == 2):
-        
+            try:
+                Path(self.path['path']).touch(mode=0o777,exist_ok=True)
+            except Exception as err:
+                print("ERROR @ findCategoryConfig: creating file: {0}"
+                      .format(err))
+            noCategoryFileLog(self.log, self.path['path'])
+            return False
+
+
+    def readConfig(self):
+        if(not(self.raw_data)):
+            return False
+        for row in self.raw_data:
+            option = "".join(row).split('=')
+            if(len(option[0]) > 1 and len(option[1]) > 1):
+                try:
+                    self.id_list[option[0]] = int(option[1])
+                except Exception as err:
+                    print("ERROR @ readCategoryConfig: convert to int: {0}"
+                          .format(err))
+            else:
+                return False
+
+        return True
+

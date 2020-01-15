321ae9d7edd69e8be0755cf5ba82289944d06ca3
Sebastian Fricke <sebastian.fricke-linux@gmx.de>
2020-01-15 15:26:09 +0100

    Refactor findConfig
    simplified the findConfig by moving the raw_data read to rawConfig,
    added a fallback to the user choosing the config if it is not found
    removed log entry function

--- packages/category.py
+++ packages/category.py
@@ -1,49 +1,56 @@
-from packages.item_upload import check_encoding
-from packages.log_files import noCategoryFileLog
+from packages.item_upload import checkEncoding
+from packages.config import get_path
 import os
 from pathlib import Path
 
 class CategoryConfig:
-    def __init__(self, proj_root, log_path):
-        self.root = proj_root
-        self.log = log_path
+    def __init__(self):
         self.id_list = dict()
         self.raw_data = []
         self.path = {'path':'', 'encoding':''}
 
-    def findConfig(self):
+    def findConfig(self, root):
         try:
-            self.path['path'] = os.path.join(self.root, 'category')
+            self.path['path'] = os.path.join(root, 'category')
         except Exception as err:
             print("ERROR @ findCategoryConfig: Building path failed: {0}"
                   .format(err))
             return False
 
         if(os.path.isfile(self.path['path'])):
-            self.path['encoding'] = check_encoding(self.path)
-            with open(self.path['path'],
-                      mode='r', encoding=self.path['encoding']) as config:
-                try:
-                    self.raw_data =\
-                    [row.stip(' ').strip('\n').split(';') for row in config]
-                except Exception as err:
-                    print("ERROR @ findCategoryConfig: Read data failed: {0}"
-                          .format(err))
-
+            self.path = check_encoding(self.path)
             return True
         else:
             try:
-                Path(self.path['path']).touch(mode=0o777,exist_ok=True)
+                self.path['path'] = get_path(message='category config path',
+                                             path_type='file',
+                                             initialdir=root)
+                self.path = check_encoding(self.path)
             except Exception as err:
-                print("ERROR @ findCategoryConfig: creating file: {0}"
+                print("ERROR @ findCategoryConfig: searching file: {0}"
                       .format(err))
-            noCategoryFileLog(self.log, self.path['path'])
-            return False
+            if(not(self.path['path'])):
+                return False
+            else:
+                return True
+
+
+    def rawConfig(self):
+        with open(self.path['path'],
+                    mode='r', encoding=self.path['encoding']) as config:
+            try:
+                self.raw_data =\
+                [row.strip(' ').strip('\n').split(';') for row in config]
+            except Exception as err:
+                print("ERROR @ findCategoryConfig: Read data failed: {0}"
+                        .format(err))
+        return len(self.raw_data) > 0
 
 
     def readConfig(self):
         if(not(self.raw_data)):
-            return False
+            if(not(self.rawConfig())):
+                return False
         for row in self.raw_data:
             option = "".join(row).split('=')
             if(len(option[0]) > 1 and len(option[1]) > 1):

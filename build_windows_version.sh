a99f6b482fe45bf4aa3c89f276d35e67924dfb3b
Sebastian Fricke <sebastian.fricke.linux@gmail.com>
2020-05-06 10:59:23 +0200

    Specify the data type of the config
    In case of multiple configs on the desktop only take the .ini
    
    Signed-off-by: Sebastian Fricke <sebastian.fricke.linux@gmail.com>

--- build_windows_version.sh
+++ build_windows_version.sh
@@ -7,7 +7,8 @@ rm -irf *
 if [ ! -f $PWD/item_upload ]; then
     echo "Build the script into a executable file using pyinstaller"
     pyinstaller -y -F -i "C:/Users/bkkas/Downloads/LogoNameless.ico" -n item_upload     "C:/Users/bkkas/Dropbox/Scripts/Item_Upload/product_import.py"
-    cp C://Desktop/config C://Users/bkkas/Dropbox/Scripts/Item_Upload/Windows_Version/config
+    cp C://Desktop/config.ini C://Users/bkkas/Dropbox/Scripts/Item_Upload/Windows_Version/config.ini
+
 fi
 
 cp ../example_config.txt Windows_version/dist/config.txt
